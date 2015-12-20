d8e2d8cd63896f335f3ba5254743f0a58b525dee
Kai Sun <tonysun329@gmail.com>
2015-12-20 22:00:00 +0800

    knows about velocity, rate, projected time remaining & on schedule (red)

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -23,25 +23,41 @@ RSpec.describe Project do
   end
 
   describe 'estimates' do
-    # Given
     let(:project) { Project.new }
-    let(:done) { Task.new(size: 2, completed_at: Time.now) }
+    let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
+    let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) }
     let(:small_not_done) { Task.new(size: 1) }
     let(:large_not_done) { Task.new(size: 4) }
 
-    # When
     before(:example) do
-      project.tasks = [done, small_not_done, large_not_done]
+      project.tasks = [newly_done, old_done, small_not_done, large_not_done]
     end
 
-    # Then
     it 'can calculate total size' do
-      expect(project.total_size).to eq(7)
+      expect(project.total_size).to eq(10)
     end
 
-    # Then
     it 'can calculate remaining size' do
       expect(project.remaining_size).to eq(5)
+    end
+
+    it 'knows its velocity' do
+      expect(project.completed_velocity).to eq(3)
+    end
+
+    it 'knows its rate' do
+      expect(project.current_rate).to eq(1.0 / 7)
+    end
+
+    it 'knows its projected time remaining' do
+      expect(project.projected_days_remaining).to eq(35)
+    end
+
+    it 'knows if it is on schedule' do
+      project.due_date = 1.week.from_now
+      expect(project).not_to be_on_schedule
+      project.due_date = 6.months.from_now
+      expect(project).to be_on_schedule
     end
   end
 end
