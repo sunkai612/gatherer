d5a49e73912eae6c231bb78f67e299d46183f286
Kai Sun <tonysun329@gmail.com>
2015-12-20 22:00:00 +0800

    can distinguish a completed task (red)

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Task do
+  it 'can distinguish a completed task' do
+    task = Task.new
+    expect(task).not_to be_complete
+    task.mark_completed
+    expect(task).to be_complete
+  end
+end
