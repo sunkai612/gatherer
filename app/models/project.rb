fb1131e1eb4a4be524cd2967d9b2083278a0472c
Kai Sun <tonysun329@gmail.com>
2015-12-20 22:00:00 +0800

    knows about velocity, rate, projected time remaining & on schedule (green)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize
     @tasks = []
@@ -19,5 +19,21 @@ class Project
 
   def remaining_size
     incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
   end
 end
